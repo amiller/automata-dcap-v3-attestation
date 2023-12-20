f058eb0c02456a62013583ee35bc7fd2c0a2630f
Mateusz Morusiewicz <11313015+Ruteri@users.noreply.github.com>
2023-12-20 18:53:17 +0100

    Stores preparesed tcbinfo and removes custom inplace parsing logic

---
+++ p3.py
@@ -0,0 +1,20 @@
+import json
+
+obj = json.load(open("contracts/assets/tcbInfo2.json"))
+
+def fixLevel(obj):
+    arr = [obj['tcb']['sgxtcbcomponents'][i]['svn'] for i in range(16)]
+    return {
+        'pcesvn': obj['tcb']['pcesvn'],
+        'sgxTcbCompSvnArr': arr,
+        'status': obj['tcbStatus']
+    }
+def fixInfo(obj):
+    return {
+        'fmspc': obj['fmspc'],
+        'pceid': obj['pceId'],
+        'tcbLevels': [fixLevel(o) for o in obj['tcbLevels']]
+    }
+
+obj['tcbInfo'] = fixInfo(obj['tcbInfo'])
+print(json.dumps(obj).replace(' ',''))
